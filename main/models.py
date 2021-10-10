c1d10305d32a76466b1ade50788253d6875f1bf1
pragyan-srivastava <67544152+pragyan-srivastava@users.noreply.github.com>
2021-10-10 22:55:49 +0530

    added parameters in models.py and forms.py

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+
 class Infra(models.Model):
     # fields for the Infra table
     name = models.CharField(max_length=300)
@@ -10,12 +12,27 @@ class Infra(models.Model):
     release_date = models.DateField()
     averageRating = models.FloatField(default=0)
     image = models.URLField(default=None, null=True)
+    electricity = models.FloatField()
+    railways = models.TextField(max_length=1000)
+    bridges = models.FloatField()
+    tunnels = models.FloatField()
+    water_supply = models.CharField(max_length=1)
+    sewers = models.FloatField()
+    telecommunication = models.TextField(max_length=5000)
+    market = models.FloatField()
+    transportation = models.TextField(max_length=5000)
+    distributors = models.TextField(max_length=5000)
+    suppliers = models.TextField(max_length=5000)
+    mining_area = models.FloatField()
+    forest_area = models.FloatField()
+    ready_to_use_land = models.FloatField()
 
     def __str__(self):
         return self.name
 
     def __unicode__(self):
         return self.name
+
 
 class Review(models.Model):
     Infra = models.ForeignKey(Infra, on_delete=models.CASCADE)
@@ -25,4 +42,3 @@ class Review(models.Model):
 
     def __str__(self):
         return self.user.username
-        
